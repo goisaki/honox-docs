72d5c21a29bcadf5cc5d9e67439e9cf30a1fd1e0
Kengo Watanabe <ken5@kwtc.jp>
2024-10-12 15:08:18 +0900

    feat: use tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+	build: {
+		rollupOptions:
+			mode === 'client'
+				? {
+						input: ['/app/styles/tailwind.css'],
+					}
+				: undefined,
+	},
 	plugins: [honox({ devServer: { adapter } }), build()],
-})
+}))
